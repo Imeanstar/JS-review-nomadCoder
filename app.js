e34472e838e8bc2d6b391cb057157ed890c413aa
Imeanstar <mike4403@ajou.ac.kr>
2025-01-31 06:54:23 +0900

    event.preventDefault 학습

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const link = document.querySelector("a");
+
 
 function onLoginSubmit(event){
     console.log(1);
@@ -9,5 +11,9 @@ function onLoginSubmit(event){
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event){
+    event.preventDefault();
+}
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
