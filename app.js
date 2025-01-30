35f6ab980a3c5f8cb44f8e32dd4403863c6af8fa
Imeanstar <mike4403@ajou.ac.kr>
2025-01-31 07:10:01 +0900

    hidden값 remove & user name 출력

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 
 function onLoginSubmit(event){
-    console.log(1);
     event.preventDefault();
-    console.log(event);
-    console.log(loginInput.value);
+    const userName = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = "Hello " + userName;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-}
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click",handleLinkClick);
