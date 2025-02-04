3dfd0e2dfb0d51f14f91312974598560a271cc00
Imeanstar <mike4403@ajou.ac.kr>
2025-02-04 16:00:55 +0900

    js,css파일 구조 변경 / index.html 연결 경로 변경 / clock.js 추가

--- js/greeting.js
+++ js/greeting.js
@@ -4,15 +4,25 @@ const loginButton = loginForm.querySelector("button");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 
 function onLoginSubmit(event){
     event.preventDefault();
     const userName = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, userName);
     loginForm.classList.add(HIDDEN_CLASSNAME);
     greeting.innerText = "Hello " + userName;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+if(savedUserName == null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else{
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `hello ${savedUserName}`;
+}
